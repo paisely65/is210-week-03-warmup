3f95a8049e838334fb60c131f3b597578da89090
paisely65 <spscuny@gmail.com>
2016-02-13 22:31:55 +0000

    Task 6 is finished

--- task_06.py
+++ task_06.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """A very long book."""
-
+  
+FHANDLER = open('war_and_peace.txt', 'r')
 import os
-
+ 
 DPATH = os.path.dirname(os.path.abspath(__file__))
-
+ 
 FHANDLER = open(os.path.join(DPATH, 'war_and_peace.txt'), 'r')
-
+  
 WORDS = FHANDLER.read()
-
-FHANDLER.close()
+WORDCT = len(WORDS.split())
+print WORDCT
