bf3054d2988a2666d7b29bd0c788c207888914fd
paisely65 <spscuny@gmail.com>
2016-03-03 02:30:27 +0000

    Task_05 has been retested

--- task_05.py
+++ task_05.py
@@ -5,6 +5,4 @@
 
 TEENAGE_MUTANT_NINJAS = ('Michaelangelo. Leonardo. Rafael. Donatello. Heroes '
                          'in a half shell.')
-TEENAGE_MUTANT_NINJAS = TEENAGE_MUTANT_NINJAS.split('.')
-TURTLE_POWER = TEENAGE_MUTANT_NINJAS
-print TURTLE_POWER
+TURTLE_POWER = TEENAGE_MUTANT_NINJAS.split('. ')
